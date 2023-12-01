69a14a01308fd0cdb1e48e542f116b1435408d58
Dhiego <dhiegofs1@gmail.com>
2023-12-01 17:20:14 -0300

    adicionado overlay

--- script.js
+++ script.js
@@ -9,7 +9,7 @@ const imagemPoster_URL = "https://image.tmdb.org/t/p/w500";
 const main = document.getElementById('main');
 const form = document.getElementById('form');
 const search = document.getElementById('search');
-const searchURL = BASE_URL+ '/search/movie?'+api_key;
+const searchURL = BASE_URL + '/search/movie?' + api_key;
 
 
 getMovies(API_URL);
@@ -23,17 +23,15 @@ function getMovies(url) {
 }
 
 
-
-
 function showMovies(data) {
     main.innerHTML = '';
 
     data.forEach(movie => {
-        const {title, poster_path, vote_average, overview} = movie;
+        const { title, poster_path, vote_average, overview, id } = movie;
         const movieEl = document.createElement('div');
         movieEl.classList.add('filme');
         movieEl.innerHTML = `
-             <img src="${imagemPoster_URL+poster_path}" alt="${title}">
+             <img class="foto_filme" src="${imagemPoster_URL + poster_path}" alt="${title}">
 
                 <div class="info-filme">
                     <h3>${title}</h3>
@@ -43,20 +41,27 @@ function showMovies(data) {
                 <div class="descricao-filme">
                     <h3>Descricao</h3>
                     ${overview}
+                 
                 </div>    
+               <button class="saiba-mais" id="${id}">Saiba Mais</button>
              `
 
         main.appendChild(movieEl); /* Coloca elemento filme no main  */
+
+        document.getElementById(id).addEventListener('click', () => {
+            console.log(id)
+            openNav(movie)
+        })
     })
 }
 
 function getColor(vote) {
     if (vote >= 8) {
-        return 'green'
+        return "green"
     } else if (vote >= 5) {
         return "orange"
     } else {
-        return 'red'
+        return "red"
     }
 }
 
@@ -65,12 +70,60 @@ form.addEventListener('submit', (e) => {
 
     const searchTerm = search.value;
 
-    if(searchTerm){
-        getMovies(searchURL+'&query='+searchTerm)
-    }else{
+    if (searchTerm) {
+        getMovies(searchURL + '&query=' + searchTerm)
+    } else {
         getMovies(API_URL)
     }
 })
+
+
+/* ------------------- OVERLAY SAIBA MAIS W3S-------------------*/
+const overlayContent = document.getElementById('overlay-content');
+
+function openNav(movie) {
+    let id = movie.id;
+    fetch(BASE_URL + '/movie/' + id + '/videos?' + api_key).then(res => res.json())
+        .then(videoData => {
+            console.log(BASE_URL + '/movie/' + id + '/videos?' + api_key)
+            console.log(videoData);
+
+            if (videoData) {
+                document.getElementById("myNav").style.width = "100%";
+                if (videoData.results.length > 0) {
+                    var embed = [];
+                    videoData.results.forEach(video => {
+                        let { name, key, site } = video;
+
+                        if (site == 'YouTube') {
+
+                            embed.push(`
+
+                            <iframe 
+                            width="560" height="315" src="https://www.youtube.com/embed/${key}" 
+                            title="${name}" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; 
+                            gyroscope; picture-in-picture; web-share" allowfullscreen>
+                            </iframe>
+                           
+                        `)
+                        }
+
+                    })
+
+                    overlayContent.innerHTML = embed.join('');
+                } else {
+                    overlayContent.innerHTML = `<h1>Nenhum resultado encontrado</h1>`
+                }
+            }
+        })
+
+
+}
+
+
+function closeNav() {
+    document.getElementById("myNav").style.width = "0%";
+}
 
 
 
